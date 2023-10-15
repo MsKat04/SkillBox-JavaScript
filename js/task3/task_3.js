f7881b2262e42a30298499e5481265479f3aedaa
MsKat04 <116561169+MsKat04@users.noreply.github.com>
2023-10-15 22:23:23 +0500

    Add files via upload

--- js/task3/task_3.js
+++ js/task3/task_3.js
@@ -1,51 +1,51 @@
 //3.1
+/*
 let password = prompt("Введите пароль");
 
 let passwordTest = password.includes('-');
 let passwordTest2 = password.includes('_');
 console.log(passwordTest);
 
-if (password.length > 3){
-    if (passwordTest || passwordTest2){
-        console.log("Пароль надёжный");
-    }
-    else{
-        console.log('Пароль недостаточно надёжный');
-    }
+if (password.length > 3 && passwordTest || passwordTest2)
+{
+    console.log("Пароль надёжный");
 }
-else {
+else
+{
     console.log('Пароль недостаточно надёжный');
 }
+*/
 
-
-
+/*
 //3.2
 let userName = prompt("введите имя");
 let userSurname = prompt("введите фамилию");
 
-let firstLetterSurname = userSurname.substring(0,1);
+let firstLetterSurname = userSurname[0];
 let othersLettersSurname = userSurname.substring(1);
 
 
-let firstLetterName = userName.substring(1);
-let othersLettersName = userName.substring(0,1);
+let othersLetterName = userName.substring(1);
+let firstLettersName = userName[0];
 
-let nameTest = (othersLettersName === othersLettersName.toUpperCase() 
-                        && firstLetterName === firstLetterName.toLowerCase()) ? true : false;
+let nameTest = (firstLettersName === firstLettersName.toUpperCase() 
+                        && othersLetterName === othersLetterName.toLowerCase());
 
 let surnameTest = (firstLetterSurname === firstLetterSurname.toUpperCase() 
-                        && othersLettersSurname === othersLettersSurname.toLowerCase()) ? true : false;
+                        && othersLettersSurname === othersLettersSurname.toLowerCase());
 
 
 let convertedName = nameTest ? console.log("Имя осталoсь без изменений") 
                     : console.log("Имя было преобразовано", 
-                        othersLettersName.toUpperCase() + firstLetterName.toLowerCase());
+                        firstLettersName.toUpperCase() + othersLetterName.toLowerCase());
 
 let convertedSurname = surnameTest ? console.log("фамилия осталaсь без изменений") 
                     : console.log("фамилия былa преобразованa", 
                         firstLetterSurname.toUpperCase() + othersLettersSurname.toLowerCase());
 
-
+*/
+/*
 //3.3
 let number = parseInt(prompt("Введите число"));
-console.log(number % 2 === 0 ? console.log('Число чётное') : console.log('Число нечётное'));
+console.log(number%2===0 ? console.log('Число чётное'): console.log('Число нечётное'));
+*/
